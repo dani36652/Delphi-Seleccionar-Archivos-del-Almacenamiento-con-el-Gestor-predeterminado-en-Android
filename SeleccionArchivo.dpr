8fc3841fba0e0b08c93ee67703711ac0d47a9f52
Daniel Rodriguez <dani36652@gmail.com>
2022-05-04 18:02:30 -0500

    Upload Files

---
+++ SeleccionArchivo.dpr
@@ -0,0 +1,14 @@
+program SeleccionArchivo;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  UPrincipal in 'UPrincipal.pas' {FrmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TFrmPrincipal, FrmPrincipal);
+  Application.Run;
+end.
